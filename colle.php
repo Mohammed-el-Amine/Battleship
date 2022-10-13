387f2c143e6de38e4c94376112c9479047451221
amine <mohammed-el-amine.djellal@epitech.eu>
2022-10-13 16:02:47 +0200

    [EDIT] ex.2: place ship in the board and error setting : error setting =>ok place ship => ko

--- colle.php
+++ colle.php
@@ -1,12 +1,35 @@
 <?php
 
-function colle(int $x, int $y)
+// function colle(int $x, int $y) // exercice 1
+
+// {
+//     $pipe = "|   ";
+//     $line = "";
+//     for ($i = 0; $i < $x; $i++) {
+//         $line .= "+---";
+//         $pipe .= "|   ";
+//     }
+
+//     $pipe .= "\n";
+//     $line .= "+";
+
+//     if ($x > 0) {
+//         for ($i = 0; $i < $y; $i++) {
+//             echo "$line\n$pipe";
+//         }
+//         echo "$line\n";
+//     }
+// }
+
+// colle(0, 0); //$x pour les $pipe ordonnee $y pour les $line absyce
+
+function colle(int $x, int $y, array $coords) // exercie 2 
+
 {
-    if($x ===0 && $y ===0) {
-        return;
-    }else{
-        $pipe = "|   ";
+    $pipe = "|   ";
     $line = "";
+    $cross = "X";
+
     for ($i = 0; $i < $x; $i++) {
         $line .= "+---";
         $pipe .= "|   ";
@@ -19,9 +42,21 @@ function colle(int $x, int $y)
         for ($i = 0; $i < $y; $i++) {
             echo "$line\n$pipe";
         }
+        echo "$line\n";
     }
-    echo "$line\n";
+
+
+    if ($coords[0][0] < 0 || $coords[0][1] < 0 || $coords[1][0] < 0 || $coords[1][1] < 0) {
+        return; //gestion du cas ou l'on essaie de mettre un navire dans une coordonée negative 
     }
+    elseif($coords[0][0] > $x || $coords[0][1]> $x || $coords[0][0] > $y || $coords[0][1]> $y || $coords[1][0] > $x || $coords[1][1]> $x || $coords[1][0] > $y || $coords[1][1]> $y  ){
+        return; // gestion du cas ou la coordonnée serait trop grande par rapport au plateau de jeu
+    }
+    else {
+        echo "$cross\n";
+    }
+    //print_r($coords);
+
 }
 
-colle(5, 5); //$x pour les $pipe ordonnee $y pour les $line absyce
+colle(5, 2, [[1, 2], [1, 1]]); //$x pour les $pipe ordonnee $y pour les $line absyce
